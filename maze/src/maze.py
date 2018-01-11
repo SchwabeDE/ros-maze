fe9a9aa6c79c22fe9ddb58d0d37fe984bb35bf51
Nico Vinzenz <nv-171738@hs-weingarten.de>
2018-01-11 15:55:00 +0100

    Adds method to classify data points

--- maze/src/maze.py
+++ maze/src/maze.py
@@ -24,8 +24,11 @@ class Robot:
 
         # Member variables
         self.vel = Twist()
-        self.odom = 0
-        self.laser = 0
+        self.odom = []
+        self.laser = []
+
+        self.phase = "SearchApproachWall"
+        self.datapointGroups = []
 
     def odomCallback(self, data):
         '''
@@ -43,11 +46,43 @@ class Robot:
         '''
         self.laser = data.ranges
 
+    def classifyDatapoints(self, ptpDistanceThreshold = 0.1):
+
+        rangeList = []
+        for idx, datapoint in enumerate(self.laser):
+            if not (rangeList):
+                rangeList.append(datapoint)
+            elif(abs(datapoint - rangeList[-1]) <= ptpDistanceThreshold):
+                rangeList.append(datapoint)
+            else:
+                avg = sum(rangeList) / float(len(rangeList))
+
+                #calculate variance
+                var = sum(map(lambda x:(x-avg)**2, rangeList))
+                #calculate standard deviation
+                std = ((1/float(len(rangeList))) * var) ** 0.5
+
+                dpGroup = [rangeList, idx-len(rangeList), avg, std]
+                self.datapointGroups.append(dpGroup)
+                rangeList = []
+        self.datapointGroups.append(len(self.laser))
+
+
     def startRobot(self):
         rospy.loginfo("start")
 
         while not rospy.is_shutdown():
-            rospy.loginfo(self.laser)
+            if(self.phase == "SearchApproachWall"):
+                while not (self.laser):
+                    rospy.loginfo("Wait for laser data..")
+
+                self.classifyDatapoints()
+
+
+            elif(self.phase == "FollowWall"):
+                pass
+            else:
+                rospy.logerr("Phasename does not match any defined phase.")
 
             # sleep to prevent flooding your console
             self.rate.sleep()
